da54c33707398b0d360d8a3469cff2ad7a647bd0
0xKjartan <theodorsson@tuta.io>
2024-06-26 16:25:51 +0000

    ripped off micrograd again

---
+++ epistemon/engine.py
@@ -0,0 +1,89 @@
+import math
+import numpy as np 
+
+class Value:
+    def __init__(self, data, _children=(), _op='', label=''):
+        self.data      = data
+        self.grad      = 0.0
+        self._backward = lambda: None
+        self._prev     = set(_children)
+        self._op       = _op
+        self.label     = label
+
+    def __repr__(self):
+        return f"Value.data={self.data}"
+
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), '+')
+        def backward():
+            self.grad  += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+
+        out._backward = backward
+        return out
+
+    def __neg__(self):
+        return self * -1
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __radd__(self, other):
+        return self + other
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), '*')
+        def backward():
+            self.grad  += other.data * out.grad
+            other.grad += self.data  * out.grad
+
+        out._backward = backward
+        return out
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supporting int/float for now"
+        out = Value(self.data**other, (self,), f'**{other}')
+        def backward():
+            self.grad += other * self.data**(other-1) * out.grad
+        out._backward = backward
+        return out
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        return self * other**-1
+
+    def tanh(self):
+        t   = (math.exp(2.0*self.data)-1.0)/(math.exp(2.0*self.data)+1.0)
+        out = Value(t, (self,), "tanh")
+        def backward():
+            self.grad += (1.0 - tanh(out.data)**2) * out.grad
+
+        out._backward = backward
+        return out
+
+    def exp(self):
+        out = Value(math.exp(self.data), (self,), 'exp')
+        def backward():
+            self.grad += out.data * out.grad
+        
+        out._backward = backward
+        return out
+
+    def backward(self):
+        self.grad = 1.0
+        topo      = []
+        visited   = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        for node in reversed(topo):
+            node._backward()
---
+++ epistemon/nn.py
@@ -0,0 +1,19 @@
+from epistemon.engine import Value
+
+class Neuron:
+    def __init__(self, nin):
+        self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
+        self.b = Value(random.uniform(-1,1))
+
+    def __call__(self, x):
+        act = sum((wi*xi for wi, xi in zip(self.w, x)), self.b)
+        out = act.tanh()
+        return out
+
+class Layer:
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+
+    def __call__(self, x):
+        outs = [n(x) for n in self.neurons]
+        return outs
